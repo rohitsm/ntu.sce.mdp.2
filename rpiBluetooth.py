41313806f1e09904409bc56f154dbddbdc171ed2
Rohit Menon <me@rohitsm.com>
2014-09-29 16:21:55 +0800

    3

--- rpiBluetooth.py
+++ rpiBluetooth.py
@@ -11,17 +11,17 @@ server_socket = BluetoothSocket( RFCOMM )
 server_socket.bind(("", btport))
 server_socket.listen(1)			# Listen for requests
 
-# port = server_socket.getsockname()[1]
+port = server_socket.getsockname()[1]
 
-# uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-# advertise_service( server_sock, "SampleServer",
-#                    service_id = uuid,
-#                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
-#                    profiles = [ SERIAL_PORT_PROFILE ],
-# 				)
+advertise_service( server_sock, "SampleServer",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ],
+				)
 
-# print "Waiting for connection on RFCOMM channel %d" % port
+print "Waiting for connection on RFCOMM channel %d" % port
 
 
 # Accept requests
