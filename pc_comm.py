250f0630d76c7e3cc5f025dc30065386ee05ac46
Rohit Menon <me@rohitsm.com>
2014-09-29 23:58:57 +0800

    PC socket connection code

--- pc_comm.py
+++ pc_comm.py
@@ -1,1 +1,27 @@
 import socket
+import sys
+
+__author__ = "Rohit"
+
+tcp_ip = "192.168.2.2" # Connecting to IP address of MDPGrp2
+port = "5143"
+
+# Create a TCP/IP socket
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+conn.bind((tcp_ip, port))
+
+# Listen for incoming connection
+conn.listen(1)
+
+client, addr = conn.accept()
+print "Connected! Connection address: ", addr
+
+pc_data = client.recv(1024)
+print "Data received: %s" %pc_data
+
+# Close the connections
+client.close()
+conn.close()
+print "end of program"
+
+
