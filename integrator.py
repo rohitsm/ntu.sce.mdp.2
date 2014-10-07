959267cd11558550f6b2deaabfa64135db42b529
Rohit Menon <rohitsm@gmail.com>
2014-10-07 22:07:49 +0800

    2

--- integrator.py
+++ integrator.py
@@ -10,7 +10,7 @@ __author__ = 'Rohit'
 # Create queues
 to_bt = Queue.Queue() 
 to_pc = Queue.Queue()
-to_sr = Queue.Queue()		// Serial queue
+to_sr = Queue.Queue()		# Serial queue
 
 if __name__ == "__main__":
 	pc_thread = PCThread()
@@ -18,13 +18,13 @@ if __name__ == "__main__":
 	sr_thread = SRThread()
 
 	# PC read and write thread
-	rt_pc = threading.Thread(target = pc_thread.readPC, args = (to_bt,), name = "pc_read_thread")
+	rt_pc = threading.Thread(target = pc_thread.readPC, args = (to_bt, to_sr,), name = "pc_read_thread")
 	print "created rt_pc"
 	wt_pc = threading.Thread(target = pc_thread.writePC, args = (to_pc,), name = "pc_write_thread")
 	print "created wt_pc"
 
 	# Bluetooth (BT) read and write thread
-	rt_bt = threading.Thread(target = bt_thread.readBT, args = (to_pc,), name = "bt_read_thread")
+	rt_bt = threading.Thread(target = bt_thread.readBT, args = (to_pc, to_sr,), name = "bt_read_thread")
 	print "created rt_bt"
 	wt_bt = threading.Thread(target = bt_thread.writeBT, args = (to_bt,), name = "bt_write_thread")
 	print "created wt_bt"
